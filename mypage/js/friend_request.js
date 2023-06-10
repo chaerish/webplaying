4ca79d813a1c98af2b71ee8216a3fe293e377874
김채연 <100480440+chaerish@users.noreply.github.com>
2023-06-10 20:26:41 +0900

    Delete mypage directory

--- mypage/js/friend_request.js
+++
@@ -1,7 +0,0 @@
-const sendRequest = document.querySelector(".send_request");
-
-function handleClick() {
-    title.style.backgroundColor = "#67BEC7";
-}
-
-sendRequest.addEventListener("click", handleClick);
