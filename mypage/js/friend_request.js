616169f9b6344b70d4b9caac5352fb413829e82d
chaerish <ccyy8432@naver.com>
2023-06-10 20:20:41 +0900

    마이페이지

---
+++ mypage/js/friend_request.js
@@ -0,0 +1,7 @@
+const sendRequest = document.querySelector(".send_request");
+
+function handleClick() {
+    title.style.backgroundColor = "#67BEC7";
+}
+
+sendRequest.addEventListener("click", handleClick);
